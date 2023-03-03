2a56c00aaa923931270f48d8b790bd4e596c0fed
Gideon Ewa <gideonewa@gmail.com>
2023-03-03 19:32:58 +0000

    Console tracing

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,13 +20,16 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
 
 # Honeycomb
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+simpleProcessor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simpleProcessor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
